df030ae3109902dde4a910cfa28b485ea0ebfaaf
Milan <rusnak36@stud.uniza.sk>
2021-12-27 14:39:56 +0100

    prvé kroky!

---
+++ Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main() {
+
+    printf("Heureka!");
+
+    return 0;
+}
--- main.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by matej on 27. 12. 2021.
-//
-
-int main( int argc, char *argv[]) {
-    cout << "Hello world!\n";
-}
